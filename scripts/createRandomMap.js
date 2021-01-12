693f88e55d6a23a80e4cd06cb241dc8310d77d42
jarkko.tenhola <jarkko.tenhola@gmail.com>
2021-01-12 21:43:20 +0200

    Small bug fixes

--- scripts/createRandomMap.js
+++ scripts/createRandomMap.js
@@ -4,7 +4,7 @@ const createRandomMap = () => {
     let width, height;
     while (true) {
         try {
-            width = Number(prompt('How wide should the map be (min 3, max 100? '));
+            width = Number(prompt('How wide should the map be (min 3, max 100)? '));
             if (!width || width < 3 || width > 100) {
                 console.log('\nPlease provide a valid input!\n');
             } else {
@@ -16,7 +16,7 @@ const createRandomMap = () => {
     }
     while (true) {
         try {
-            height = Number(prompt('How tall should the map be (min 3, max 20? '));
+            height = Number(prompt('How tall should the map be (min 3, max 20)? '));
             if (!height || height < 3 || height > 20) {
                 console.log('\nPlease provide a valid input!\n');
             } else {
